c18ed4bee431dac808feec5b42f6effb083f4421
JMaria <jmariadelacruzsanchez@gmail.com>
2022-05-20 15:29:59 +0200

    testing script

--- deploy/03-testRacksItems.js
+++ deploy/03-testRacksItems.js
@@ -11,10 +11,28 @@
         // FUNCTION TESTING
 
         //1st -> Change casePrice
-        const racksTokenMintTx = await racksItems.mint(deployer, BigInt(racksTokenQuantity) * decimals, {gasLimit: 9999999})
-        console.log("Minting " + racksTokenQuantity + " RacksTokens to wallet: " + deployer);
-        const racksTokenMintTxReceipt = await racksTokenMintTx.wait(1)
+        console.log("CHANGE CASE PRICE TEST")
+        const decimals = 1000000000000000000n; // 10e18 
+        const newCasePrice = 20; // 20 RacksTokens
+        const setCasePricetTx = await racksItems.setCasePrice(BigInt(20) * decimals, {gasLimit: 9999999})
+        console.log("Changing case price to " + setCasePricetTx + "...")
+        const racksTokenMintTxReceipt = await setCasePricetTx.wait(1)
+        console.log("Case price changed to " + newCasePrice)
 
+        //2st -> mint item
+        console.log("MINT ITEM TEST")
+        const mintItemTx = await racksItems.listItem(2, {gasLimit: 9999999});
+        console.log("Minting 2 items to contract address...")
+        const mintItemTxReceipt = await mintItemTx.wait(1);
+        console.log("Minted!");
+
+        //3st -> set itemUri
+        console.log("SET ITEM URI TEST")
+        var testTokenUri = { "id": 0, "name": "item0" , "image": ""}
+        const setUriTx = await racksItems.setTokenUri(testTokenUri,{gasLimit: 9999999})
+        console.log("Setting tokenUri...");
+        const setUriTxReceipt = await setUriTx.wait(1);
+        console.log("Uri set!")
     }
     
     module.exports.tags = ["all", "testRacksItems"]
